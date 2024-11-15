a239a4eac050877d1eacb9e83c747e82b5aaec13
Lucas Almeida Coelho <lucas.acoelho@hotmail.com>
2024-11-15 17:04:53 -0300

    Updates about sound
    sound Efects!
    and a mobile build

--- 3D doding game/Assets/Scripts/Player.cs
+++ 3D doding game/Assets/Scripts/Player.cs
@@ -3,45 +3,49 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
     public float vel;
     public Rigidbody player;
-    public Collider ground;
-    public Collider ground1;
-    public Boolean Injump = false;
+    public GameObject dieText;
+    public GameObject restart;
+    public bool Ispaused = false;
+    public FixedJoystick Joystick;
+    public AudioSource bruh;
+    public AudioSource background;
+    
     
 
     // Start is called before the first frame update
     void Start()
     {
-         
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         float H = Input.GetAxis("Horizontal");
-        transform.Translate(new Vector3(H * Time.deltaTime, 0, 0));
-
-        float V = Input.GetAxis("Vertical");
-        transform.Translate(new Vector3(0, 0, V * Time.deltaTime));
-        if (Input.GetKeyDown("space")& Injump == false)
-        {
-            Injump = true;
-            StartCoroutine(Skilldelay());
-            player.AddForce(new Vector3(0, 10f, 0), ForceMode.Impulse);
-                    
-                    
-        }
-
+        transform.Translate(new Vector3((H)* Time.deltaTime, 0, 0));
+        float J = Joystick.Horizontal * vel;
+        transform.Translate(new Vector3(J * Time.deltaTime,0, 0));
     }
-
-    public IEnumerator Skilldelay()
+    public void OnCollisionEnter(Collision death)
     {
-        yield return new WaitForSeconds(1);
-        Injump = false;
+        if (death.gameObject.tag == "Stone")
+        {
+            background.Pause();
+            bruh.Play();
+            Ispaused = true;
+            dieText.SetActive(true);
+            restart.SetActive(true);
+            print("hit");
+        }
+        
     }
+    
         
 }
--- 3D doding game/Assets/Scripts/spawers.cs
+++ 3D doding game/Assets/Scripts/spawers.cs
@@ -1,23 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class spawers : MonoBehaviour
 {
     public GameObject Sphereprefab;
     public float elapsedTime = 0;
-    
+    public GameObject spawner1;
+    public float timer1;
+    public GameObject spawner2;
+    public float timer2;
+    public GameObject spawner3;
+
+    public  Player player;
+
     // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
-
-        if (elapsedTime > 3)
+        if (player.Ispaused == true)
         {
-            elapsedTime = 0;
-            Instantiate(Sphereprefab, transform.position, Quaternion.identity);
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Space pressed");
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                player.Ispaused = false;
+            }
         }
+        else
+        {
+            elapsedTime += Time.deltaTime;
+
+            if (elapsedTime > 3)
+            {
+                elapsedTime = 0;
+                Instantiate(Sphereprefab, spawner1.transform.position, Quaternion.identity);
+            }
+            timer1 += Time.deltaTime;
+
+            if (timer1 > 4)
+            {
+                timer1 = 0;
+                Instantiate(Sphereprefab, spawner2.transform.position, Quaternion.identity);
+            }
+            timer2 += Time.deltaTime;
+
+            if (timer2 > 2)
+            {
+                timer2 = 0;
+                Instantiate(Sphereprefab, spawner3.transform.position, Quaternion.identity);
+            }
+        }
+        
 
     }
 
